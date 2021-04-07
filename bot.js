cd30bab023d2a2e48bb16fc9ff026f6355f2e9b3
Diego Atravesar <diegoatravesar@gmail.com>
2021-04-07 13:34:24 -0400

    Added full server pulling for the bot
    * The auth.json file now has a server section. Enter the Server ID you wish to pull from and the webhook to output to.
    * This works the same way the channels does.

--- bot.js
+++ bot.js
@@ -10,6 +10,7 @@ bot.login(auth.token);
 var request = require("request");
 
 var channels = auth.channels;
+var servers = auth.servers;
 var consoleOutput = auth.consoleOutput;
 
 // Debug level of logger outputs EVERYTHING
@@ -19,6 +20,7 @@ if(consoleOutput){
     // 0 level of logger outputs nothing
     logger.level = '0';
 }
+var servArr = [];
 var chanArr = [];
 var relay = true;
 var timeout;
@@ -30,6 +32,7 @@ bot.on('ready', function () {
     bot.user.setStatus('invisible')
         .then(console.log)
         .catch(console.error);
+    // Generate all channel related data
     var channelArr = bot.channels.cache.array();
     console.log(`\nAvailable channels:\n`);
     for (i in channelArr) {
@@ -38,8 +41,8 @@ bot.on('ready', function () {
     console.log(`\n\nListening to:\n`);
     for (i in channels) {
         if (channels[i].name) {
-            if (bot.channels.cache.exists('name', channels[i].name)) {
-                let tmpChan = bot.channels.cache.find('name', channels[i].name);
+            if (bot.channels.cache.has(channels[i].name)) {
+                let tmpChan = bot.channels.cache.get(channels[i].name);
                 channels[i].id = tmpChan.id;
                 console.log(`Found channel ${channels[i].name} with ID ${channels[i].id}`);
                 chanArr.push(channels[i].id);
@@ -64,6 +67,21 @@ bot.on('ready', function () {
                     .catch(console.error);*/
             } else {
                 logger.warn(`Could not find channel name for id ${channels[i].id}`);
+            }
+        }
+    }
+    
+    // generate the list of servers to grab data from
+    for (i in servers) {
+        if (servers[i].id) {
+            // If the bot is connected to a server with a matching id
+            if(bot.guilds.cache.has(servers[i].id)){
+                let tmpServ = bot.guilds.cache.get(servers[i].id);
+                servers[i].name = tmpServ.name;
+                console.log(`ID - Found server ${servers[i].name} with ID ${servers[i].id}`);
+                servArr.push(servers[i].id);
+            }else {
+                logger.warn(`Could not find server name for id ${servers[i].id}`);
             }
         }
     }
@@ -107,7 +125,110 @@ bot.on('guildMemberUpdate', function (oldMember, newMember) {
 bot.on('message', function (message) {
     logger.debug(`[${message.guild.name}] [${message.channel.name}] [${message.author.username}] > ${message.content}`);
     
-    // This code checks for the FIRST instance of the Channel ID in the webhook list
+    // This code checks for the FIRST instance of the Server ID in the server list
+    if(servArr.indexOf(message.guild.id) > -1){
+        if(relay){
+            // logger.debug('==== DEBUG ====');
+            // logger.debug(util.inspect(message.attachments));
+            // logger.debug(util.inspect(message.embeds));
+            // logger.debug(message.type);
+            // logger.debug('===============');
+            
+            // _.Find will only pull the FIRST result. We want ALL results.
+            //var obj = _.find(channels, function (obj) { return obj.id === message.channel.id; });
+            
+            //_.Filter will return ALL results. This gets us ALL Webhooks to output to.
+            var servObj = _.filter(servers, function (servObj) { return servObj.id === message.guild.id; });
+            
+            var post_data = {};
+            // This posts the message under the SERVER name
+            //post_data.username = message.guild.name;
+            
+            // This will post the message using special formatting
+            if(message.member.nickname != null){
+               post_data.username = `[${message.guild.name}][${message.channel.name}][${message.member.nickname}]`;
+            } else {
+                post_data.username = `[${message.guild.name}][${message.channel.name}][${message.member.user.tag}]`;
+            }
+            
+            post_data.avatar_url = message.author.displayAvatarURL();
+
+            if (message.content && message.content != '') {
+                logger.info(`$`);
+                post_data.content = `${message.content}`
+            }
+
+            if (message.embeds.length > 0) {
+                logger.debug('==== DEBUG ====');
+                logger.debug(util.inspect(message.embeds));
+                logger.debug('===============');
+                var embed = message.embeds[0];
+                delete embed['message'];
+                delete embed['createdTimestamp'];
+                if (embed['image']) {
+                    delete embed['image']['embed'];
+                    delete embed['image']['proxyURL'];
+                    delete embed['image']['height'];
+                    delete embed['image']['width'];
+                }
+                if (embed['video'])
+                    delete embed['video'];
+                if (embed['provider'])
+                    delete embed['provider'];
+                if (embed['fields'].length < 1)
+                    delete embed['fields'];
+                for (var propName in embed) {
+                    if (embed[propName] === null || embed[propName] === undefined || embed[propName] == []) {
+                        delete embed[propName];
+                    }
+                }
+                logger.debug(embed);
+                var embedTest = { "color": "#3AA3E3", "fields": [{ "name": "name", "value": "value", "inline": false }, { "name": "name", "value": "value", "inline": true }] };
+                post_data.embeds = [embed];
+            }
+
+            var attachArray = message.attachments.array();
+            if (attachArray.length > 0) {
+                // logger.debug(util.inspect(attachArray));
+                var attach = attachArray[0];
+                logger.debug(attach.url);
+                post_data.content += '\n' + attach.url;
+            }
+
+            // This will loop through ALL output webhooks and send a request for each.
+            for(var i = 0; i < servObj.length; i++){
+                //This code creates the data for the request
+                var url = servObj[i].webhook;
+                var options = {
+                    method: 'post',
+                    body: post_data,
+                    json: true,
+                    url: url
+                };
+
+                // This code sends the data to the webhook
+                request(options, function (err, res, body) {
+                    if (err) {
+                        console.error('error posting json: ', err)
+                        throw err
+                    }
+                    
+                    var headers = res.headers
+                    var statusCode = res.statusCode
+                    //console.log('headers: ', headers)
+                    if(consoleOutput) console.log('[STATUS] > Sent ', statusCode)
+                    //console.log('body: ', body)
+                });
+            }
+        } else {
+            logger.warn('==== WARN ====');
+            logger.warn(`NOT SENDING MESSAGE DUE TO RELAY PROTECTION`);
+            logger.warn(`[${message.guild.name}] [${message.channel.name}] [${message.author.username}] > ${message.content}`);
+            logger.warn('==============');
+        }
+    }
+    
+    // This code checks for the FIRST instance of the Channel ID in the channel list
     if (chanArr.indexOf(message.channel.id) > -1) {
         if (relay) {
             // logger.debug('==== DEBUG ====');
